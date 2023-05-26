bde906ba19f84d834a1dc7cb1dc0bbd1a979b889
marchandashaza <133954133+marchandashaza@users.noreply.github.com>
2023-05-26 15:00:52 +0800

    add class university

--- Source1.cpp
+++ Source1.cpp
@@ -1,3 +1,100 @@
+class University 
+{
+    University * head; University * tail;
+
+public:
+    int rank;
+	string institution;
+	string LocationCode;
+	string Location;
+	double ArScore;
+	int ArRank;
+	double ErScore;
+	int ErRank;
+	double FsrScore;
+	int FsrRank;
+	double CpfScore;
+	int CpfRank;
+	double IfrScore;
+	int IfrRank;
+	double IsrScore;
+	int IsrRank;
+	double IrnScore;
+	int IrnRank;
+	double GerScore;
+	int GerRank;
+	double ScoreScaled;
+	University* nextAdd;
+	University* prevAdd;
+	SingleLinkedList<University> univDLL;
+	DoubleLinkedList<University> univDLL;
+
+    University(int rank, string institution, string LocationCode, string Location, double ArScore, int ArRank,
+		double ErScore, int ErRank, double FsrScore, int FsrRank, double CpfScore, int CpfRank, double IfrScore, int IfrRank, double IsrScore,
+		int IsrRank, double IrnSCore, int IrnRank, double GerScore, int GerRank, double ScoreScaled) 
+    {
+		this->rank = rank;
+		this->institution = institution;
+		this->LocationCode = LocationCode;
+		this->Location = Location;
+		this->ArScore = ArScore;
+		this->ArRank = ArRank;
+		this->ErScore = ErScore;
+		this->ErRank = ErRank;
+		this->FsrScore = FsrScore;
+		this->FsrRank = FsrRank;
+		this->CpfScore = CpfScore;
+		this->CpfRank = CpfRank;
+		this->IfrScore = IfrScore;
+		this->IfrRank = IfrRank;
+		this->IsrScore = IsrScore;
+		this->IsrRank = IsrRank;
+		this->IrnScore = IrnSCore;
+		this->IrnRank = IrnRank;
+		this->GerScore = GerScore;
+		this->GerRank = GerRank;
+		this->ScoreScaled = ScoreScaled;
+		this->nextAdd = NULL;
+		this->prevAdd = NULL;
+	}
+    University() 
+    {
+		this->rank = NULL;
+		this->institution = "";
+		this->LocationCode = "";
+		this->Location = "";
+		this->ArScore = NULL;
+		this->ArRank = NULL;
+		this->ErScore = NULL;
+		this->ErRank = NULL;
+		this->FsrScore = NULL;
+		this->FsrRank = NULL;
+		this->CpfScore = NULL;
+		this->CpfRank = NULL;
+		this->IfrScore = NULL;
+		this->IfrRank = NULL;
+		this->IsrScore = NULL;
+		this->IsrRank = NULL;
+		this->IrnScore = NULL;
+		this->IrnRank = NULL;
+		this->GerScore = NULL;
+		this->GerRank = NULL;
+		this->ScoreScaled = NULL;
+		this->nextAdd = NULL;
+		this->prevAdd = NULL;
+	}
+    void insertToEndList(string rank, string institution, string LocationCode, string Location, string ArScore, string ArRank,
+		string ErScore, string ErRank, string FsrScore, string FsrRank, string CpfScore, string CpfRank, string IfrScore, string IfrRank, string IsrScore,
+		string IsrRank, string IrnSCore, string IrnRank, string GerScore, string GerRank, string ScoreScaled);
+};
+
+
+
+
+
+
+
+
 int main()
 {
     head = NULL;
