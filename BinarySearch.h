977370a5dfd513f97181c4ab50e615db0f4c6dbf
marchandashaza <133954133+marchandashaza@users.noreply.github.com>
2023-05-30 21:50:58 +0800

    Update BinarySearch.h

--- BinarySearch.h
+++ BinarySearch.h
@@ -1,18 +1,150 @@
-// #include <iostream>
-// #include <algorithm>
-// #include <vector>
+#pragma once
+#include <iostream>
 
-// int main() {
-//     std::vector<int> numbers = {1, 3, 5, 7, 9};
+using namespace std;
 
-//     bool exists = std::binary_search(numbers.begin(), numbers.end(), 5);
+template <class Node>
+Node* midNode(Node* front, Node* back){
+	if(front != NULL){
+		return nullptr;
+	}
+	Node* left = front;
+	Node* right = front->nextAdd;
+	
+	while(right != back){
+		right = right->nextAdd;
+		if(right != back && right != NULL){
+			left = left->nextAdd;
+			right = right->nextAdd;
+		}
+	}
+	return left;
+}
 
-//     if (exists) {
-//         std::cout << "The element exists in the vector.\n";
-//     } else {
-//         std::cout << "The element does not exist in the vector.\n";
-//     }
 
-//     return 0;
-// }
+template <class Node>
+Node* NormalBinarySearch(Node* head, string target) {
+	T* begin = head;
+	T* finish = NULL;
 
+	while (begin != finish) {
+		T* mid = midNode(begin, finish);
+		if (data.compare(mid->institution) == 0) {
+			return mid;
+		}
+		else if (mid->institution.compare(target) > 0) {
+			finish = mid;
+		}
+		else {
+			begin = mid->nextAdd;
+		}
+	}
+	
+	return NULL;
+}
+
+
+template <class Node>
+Node* MemberBinarySearch(T* head, string target, int type) {
+    T* begin = head;
+    T* finish = nullptr;
+
+    switch (type) {
+        case 1:
+            while (begin != finish) {
+                T* mid = middle(begin, finish);
+                if (stoi(target) == mid->rank) {
+                    return mid;
+                }
+                else if (mid->rank > stoi(target)) {
+                    finish = mid;
+                }
+                else {
+                    begin = mid->nextAdd;
+                }
+            }
+            break;
+
+        case 2:
+            while (begin != finish) {
+                T* mid = middle(begin, finish);
+                if (target.compare(mid->institution) == 0) {
+                    return mid;
+                }
+                else if (mid->institution.compare(target) > 0) {
+                    finish = mid;
+                }
+                else {
+                    begin = mid->nextAdd;
+                }
+            }
+            break;
+            
+        case 3:
+            while (begin != finish) {
+                T* mid = middle(begin, finish);
+                if (target.compare(mid->LocationCode) == 0) {
+                    return mid;
+                }
+                else if (mid->LocationCode.compare(target) > 0) {
+                    finish = mid;
+                }
+                else {
+                    begin = mid->nextAdd;
+                }
+            }
+            break;
+            
+        case 4:
+            while (begin != finish) {
+                T* mid = middle(begin, finish);
+                if (target.compare(mid->Location) == 0) {
+                    return mid;
+                }
+                else if (mid->Location.compare(target) > 0) {
+                    finish = mid;
+                }
+                else {
+                    begin = mid->nextAdd;
+                }
+            }
+            break;
+        
+        case 5:
+            while (begin != finish) {
+                T* mid = middle(begin, finish);
+                if (stoi(target) == mid->ArRank) {
+                    return mid;
+                }
+                else if (mid->ArRank > stoi(target)) {
+                    finish = mid;
+                }
+                else {
+                    begin = mid->nextAdd;
+                }
+            }
+            break;
+            
+        case 6:
+            while (begin != finish) {
+                T* mid = middle(begin, finish);
+                if (stoi(target) == mid->ErRank) {
+                    return mid;
+                }
+                else if (mid->ErRank > stoi(target)) {
+                    finish = mid;
+                }
+                else {
+                    begin = mid->nextAdd;
+                }
+            }
+            break;
+            
+        
+
+        default:
+            return nullptr;
+    }
+
+    return nullptr;
+};
